8b22ec0a1902bb13889a771945a2f518325fda80
Sebastian Basner <sebastian.basner@gmx.de>
2014-04-12 16:26:04 +0200

    basic request structure

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,16 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    //Ok(views.html.index("Your new application is ready."))
+    //Ok("Hello World")
+    //Ok("Hello World)
+    Redirect(routes.Application.discs)
   }
+
+  def discs = TODO
+
+  def newDisc = TODO
+
+  def deleteDisc(id: Long) = TODO
 
 }
